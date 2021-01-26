7dba5150a4fbbf21983425b02d34e39b7c083924
Herma <hermannsterling@gmail.com>
2021-01-26 04:57:46 -0500

    Add login layout

---
+++ app/src/main/java/com/example/rb_assign1_repo/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.rb_assign1_repo
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_layout)
+
+
+    }
+}
+
